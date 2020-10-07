6333455f918a4f6ccd11d867168bfacbe3333416
motform <love.lagerkvist@protonmail.com>
2020-10-07 16:30:58 +0200

    Add basic re-frame/shadow-cljs structure

--- src/amr/app/core.cljs
+++ src/amr/app/core.cljs
@@ -1,3 +1,17 @@
-(ns amr.app.core)
+(ns amr.app.core
+  (:require [re-frame.core :as rf]
+            [reagent.dom :as r]
+            [goog.dom :as gdom]))
 
-(defn ^:export mount [])
+(enable-console-print!) 
+
+(defn render []
+  (r/render [:h1 "Parliament of Species!"]
+            (gdom/getElement "mount")))
+
+(defn ^:dev/after-load clear-cache-and-render! []
+  (rf/clear-subscription-cache!)
+  (render))
+
+(defn ^:export mount []
+  (render))
