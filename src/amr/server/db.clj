da6c84dbf24176f665405fb150bdff5d67714db0
motform <love.lagerkvist@protonmail.com>
2020-10-07 18:23:10 +0200

    Add db skeleton

--- src/amr/server/db.clj
+++ src/amr/server/db.clj
@@ -1,2 +1,11 @@
-(ns amr.server.db)
+(ns amr.server.db
+  (:require [amr.config :refer [config]]
+            [clojure.edn :as edn]
+            [datomic.client.api :as d]))
+
+(def schema (-> "resources/edn/schema.edn" slurp edn/read-string))
+
+(def client (d/client (get-in config [:datomic :cfg])))
+
+(def conn (d/connect client {:db-name "amr"}))
 
