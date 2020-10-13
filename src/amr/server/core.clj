b1c17e32c413ab2d1134d99a2041aeceb93865e7
motform <love.lagerkvist@protonmail.com>
2020-10-13 19:41:27 +0200

    Add game routes

--- src/amr/server/core.clj
+++ src/amr/server/core.clj
@@ -1,27 +1,32 @@
 (ns amr.server.core
-  (:require [amr.server.routes :as routes]
-            [amr.config :refer [config]]
+  (:require [amr.config :refer [config]]
+            [amr.server.routes :as routes]
+            [muuntaja.core :as m]
             [reitit.dev.pretty :as pretty]
             [reitit.ring :as ring]
             [reitit.ring.middleware.dev :as dev]
-            [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.ring.middleware.exception :as exception]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
             [reitit.ring.middleware.parameters :as parameters]
             [reitit.spec :as spec]
             [ring.adapter.jetty :as jetty]
-            [muuntaja.core :as m]))
+            [ring.middleware.cors :as cors]))
 
 (def server
   (ring/ring-handler
    (ring/router
 
-    [["/api" routes/ping]]
+    [["/api"
+      routes/game]]
 
     {:exception pretty/exception
      :validate spec/validate
      :reitit.middleware/transform dev/print-request-diffs
      :data {:muuntaja m/instance
-            :middleware [parameters/parameters-middleware
+            :middleware [[cors/wrap-cors
+                          :access-control-allow-origin [#"http://localhost:8022"]
+                          :access-control-allow-methods [:get :put :post :delete]]
+                         parameters/parameters-middleware
                          muuntaja/format-negotiate-middleware
                          muuntaja/format-response-middleware
                          exception/exception-middleware
@@ -29,15 +34,20 @@
 
    (ring/create-default-handler)))
 
-(defn start! []
+(defn -main []
   (let [port (or (get-in config [:server :port]) 3000)]
     (jetty/run-jetty #'server {:port port :join? false})
-    (println "Server running at " port)))
+    (println "Server running at" port)))
 
 (comment
 
   (server {:request-method :get
-           :uri "/api/ping" :query-params {:e "pong"}})
+           :headers {"Accept" "application/transit+json"}
+           :uri "/api/projection/random"})
+
+  (server {:request-method :get
+           :headers {"Accept" "application/transit+json"}
+           :uri "/api/projection/id/a975be9f-6ab6-4df1-8036-57a5be9ecb13"})
 
   )
 
--- src/amr/server/routes.clj
+++ src/amr/server/routes.clj
@@ -1,7 +1,38 @@
-(ns amr.server.routes)
+(ns amr.server.routes
+  (:require [amr.server.db :as db])
+  (:import [java.util UUID]))
 
-(def ping
-  [["/ping" {:name ::ping
-             :get (fn [{{:keys [e]} :query-params}]
-                    {:status 200 :body {:ping e}})}]])
+(def game
+  [["/projection"
+    ["/id/{id}"
+     {:name :api.projection/id
+      :get (fn [{{:keys [id]} :path-params}]
+             {:status 200
+              :body (db/id->e (UUID/fromString id) :projection)})}]
+    ["/random"
+     {:name :api.projection/random
+      :get (fn [_]
+             {:status 200
+              :body (db/random :projection)})}]]
 
+   ["/policy"
+    ["/id/{id}"
+     {:name :api.policy/id
+      :get (fn [{{:keys [id]} :path-params}]
+             {:status 200
+              :body (db/id->e (UUID/fromString id) :policy)})}
+     ["/projection/{projection-id}"
+      {:name :api.policy/projection
+       :doc "Returns a random policy connected to the projection"
+       :get (fn [{{:keys [projection-id]} :path-params}]
+              {:status 200
+               :body (db/projection->policy (UUID/fromString projection-id) :policy)})}]]
+
+    ["/effect"
+     ["/id/{id}"
+      {:name :api.policy/id
+       :get (fn [{{:keys [id]} :path-params}]
+              {:status 200
+               :body (db/id->e (UUID/fromString id) :effect)})}]]
+
+    ]])
