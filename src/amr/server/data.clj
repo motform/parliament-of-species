68bdcd5675e567fe7ff3ca902c156bd6b8ed3480
motform <love.lagerkvist@protonmail.com>
2020-10-14 16:40:05 +0200

    Handle data imports for refs and enums

--- src/amr/server/data.clj
+++ src/amr/server/data.clj
@@ -1,36 +1,54 @@
 (ns amr.server.data
   (:require [amr.utils :as utils]
             [clojure.data.csv :as csv]
-            [clojure.string :as str])
-  (:import [java.util UUID]))
+            [clojure.string :as str]))
+
+;;; HELPERS 
 
 (defn csv->m [domain [header & rest]]
   (map zipmap
        (->> header (map (partial keyword (name domain))) repeat)
        rest))
 
-(defn ->UUID
-  ([s] 
-   (when s (UUID/fromString s)))
-  ([m ks]
-   (utils/update-vals m ks ->UUID)))
+(defn ->lookup-ref [v ref-k]
+  [ref-k v])
+
+(defn ->tag [s]
+  (keyword "tag" (str/replace s " " "-")))
+
+(defn derived? [policy]
+  (:policy/derived policy))
+
+;;; TRANSFORMATION
 
 (defmulti xf-row :domain)
 
 (defmethod xf-row :projection [{:keys [row]}]
   (-> row
-      (->UUID [:projection/id])
+      (utils/->UUID [:projection/id])
       (update :projection/source #(str/split % #";"))))
 
 (defmethod xf-row :policy [{:keys [row]}]
   (-> row
-      (->UUID [:policy/id :policy/projection :policy/session])
-      (utils/?update :policy/derived    ->UUID)))
+      (utils/->UUID [:policy/id :policy/projection :policy/session])
+      (update :policy/projection ->lookup-ref :projection/id)
+      (update :policy/session ->lookup-ref :session/id)
+      (utils/?update :policy/derived utils/->UUID)
+      (utils/?update :policy/derived ->lookup-ref :policy/id)))
 
 (defmethod xf-row :session [{:keys [row]}]
   (-> row
-      (->UUID [:session/id])
+      (utils/->UUID [:session/id])
       (update :session/entity utils/->entity)))
+
+(defmethod xf-row :effect [{:keys [row]}]
+  (-> row
+      (utils/->UUID [:effect/id :effect/policy :effect/session])
+      (update :effect/policy  ->lookup-ref :policy/id)
+      (update :effect/session ->lookup-ref :session/id)
+      (update :effect/impact #(keyword "impact" %))
+      (update :effect/tag #(str/split % #";"))
+      (update :effect/tag #(map ->tag %))))
 
 (defn parse-csv
   "`Domain` is a datomic entity ns."
@@ -43,7 +61,8 @@
        (map #(xf-row {:domain domain :row %}))))
 
 (comment
-  (parse-csv "resources/csv/policies.csv"    :policy)
+  (remove derived? (parse-csv "resources/csv/policies.csv" :policy))
   (parse-csv "resources/csv/projections.csv" :projection)
-  (parse-csv "resources/csv/sessions.csv"    :session)
+  (parse-csv "resources/csv/sessions.csv" :session)
+  (parse-csv "resources/csv/effects.csv" :effect)
   )
--- src/amr/server/db.clj
+++ src/amr/server/db.clj
@@ -3,92 +3,117 @@
             [amr.server.data :as data]
             [clojure.edn :as edn]
             [datomic.client.api :as d]
-            [datomic.api :as dm]))
+            [amr.utils :as utils]
+            [amr.server.db :as db]))
 
 ;; TODO figure out how to include the entity-enums
 
-;; Move to integrant?
+;;; SETUP ;; NOTE maybe move this to a component library?
+
 (def schema (-> "resources/edn/schema.edn" slurp edn/read-string))
 (def client (d/client (get-in config [:datomic :cfg])))
 (def conn (d/connect client {:db-name "amr"}))
 (def db (d/db conn))
 
+;;; HELPERS
+
+(defn- q-rand [arg-map]
+  (first (rand-nth (d/qseq arg-map))))
+
 ;;; QUERIES
 
-(defn id->e [id k]
-  (let [ns (keyword (name k) "id")]
+(defn id->e
+  "Return the entity with `id` in domain `k`."
+  [id k]
+  (let [domain (keyword (name k) "id")]
     (d/q {:query '[:find (pull ?e [*])
                    :where [?e ?ns ?id]
                    :in $ ?id ?ns]
-          :args [db id ns]})))
+          :args [db id domain]})))
 
-(defn random [k]
+(defn random
+  "Return random entity from domain `k`."
+  [k]
   (let [ns (keyword (name k) "id")]
-    (first (rand-nth (d/qseq {:query '[:find (pull ?e [*])
-                                       :where [?e ?ns _]
-                                       :in $ ?ns]
-                              :args [db ns]})))))
+    (q-rand {:query '[:find (pull ?e [*])
+                      :where [?e ?ns _]
+                      :in $ ?ns]
+             :args [db ns]})))
 
-(defn projection [id]
-  (d/pull db '[*] (id->e id "projection")))
-
-(defn projection->policy [])
-
+(defn policy-for-entity
+  "Returns a random policy for `projection` _not_ written by the `entity`"
+  [projection entity]
+  (q-rand {:query '[:find (pull ?policy [*])
+                    :where
+                    [?policy :policy/session ?session]
+                    (not [?session :session/entity ?entity])
+                    [?e :policy/projection ?projection]
+                    [?projection :projection/id ?projection-id]
+                    :in $ ?projection-id ?entity]
+           :args [db projection (utils/->entity entity)]}))
 
 
 (comment
-
-  (d/q {:query '[:find ?n
-                 :where [?e :projection/entities :entity/aqua]
-                 [?e :projection/name ?n]]
-        :args [db]})
-
-  (dm/connect )
-  ;; DEV LOCAL
+  ;;; DEV LOCAL
   (d/transact conn {:tx-data schema})
-
-  (d/transact conn {:tx-data [{:db/ident :projection/entities
-                               :db/valueType :db.type/ref}]})
-
   (d/transact conn {:tx-data (data/parse-csv "resources/csv/projections.csv" :projection)})
 
-  (d/delete-database client {:db-name "amr"})
+  ;;; Testing in-memory with peer library
+  (require '[datomic.api :as dm])
 
-  (id->e #uuid "a975be9f-6ab6-4df1-8036-57a5be9ecb13" :projection)
-
-  ;; MEM
   (def muri "datomic:mem://amr.server/repl")
 
   (do (dm/delete-database muri)
       (dm/create-database muri)
-      (def mconn (dm/connect muri))
-      (def mdb (dm/db mconn))
-      (dm/transact mconn schema))
-
-  (dm/transact mconn (data/parse-csv "resources/csv/projections.csv" :projection))
-
+      (def mconn (dm/connect muri)))
   
-  (dm/q '[:find ?n
+  (defn transact-singularly [data]
+    (doall (map #(dm/transact mconn [%]) data)))
+
+  (do (dm/transact mconn schema)
+      (dm/transact mconn (data/parse-csv "resources/csv/projections.csv" :projection))
+      (dm/transact mconn (data/parse-csv "resources/csv/sessions.csv" :session))
+
+      ;; First transact non-derived polices, then the derived ones
+      (transact-singularly (remove data/derived? (data/parse-csv "resources/csv/policies.csv" :policy)))
+      (transact-singularly (filter data/derived? (data/parse-csv "resources/csv/policies.csv" :policy)))
+      (transact-singularly (data/parse-csv "resources/csv/effects.csv" :effect))
+      (def mdb (dm/db mconn)))
+
+  ;;; QUERIES 
+
+  ;; count the impact
+  (dm/q '[:find ?i
+          :with ?impact
           :where
-          [?e :projection/name ?n]
-          [?e :projection/entities :entity/homo-sapiens]]
+          [?impact :effect/impact ?i]]
         mdb)
 
-  #{[17592186045428 #uuid "a975be9f-6ab6-4df1-8036-57a5be9ecb13"]
-    [17592186045427 #uuid "31d5db74-cd3d-42d3-ad50-cf703b3f4143"]}
+  (dm/q '[:find ?n
+          :where
+          [?pol :policy/id  #uuid "189d2c8f-71f8-41bd-bbb1-d3980eecdd96"]
+          [?pol :policy/session ?pro]
+          [?pro :session/entity ?n]]
+        mdb)
 
-  (dm/pull mdb '[*] 17592186045427)
+  (dm/q '[:find (pull ?e [*])
+          :where
+          [?e :policy/session ?s]
+          (not [?s :session/entity ?entity])
+          [?e :policy/projection ?p]
+          [?p :projection/id ?projection-id]
+          :in $ ?projection-id ?entity]
+        mdb #uuid "a975be9f-6ab6-4df1-8036-57a5be9ecb13" (utils/->entity "fauna"))
 
-  {:db/id 17592186045427,
-   :projection/id #uuid "31d5db74-cd3d-42d3-ad50-cf703b3f4143",
-   :projection/source ["Kristoffer" "React"],
-   :projection/name "food industry",
-   :projection/text "Bacteria can spread direct or indirect from food to humans via the food chain.\nBecause countries have different regulations on the use of antibiotics in the food industry,
- more and more people have started turning to their local farmers,
- boycotting food from abroad.\n",
-   :projection/entities [#:db{:id 17592186045417}
-                         #:db{:id 17592186045418}
-                         #:db{:id 17592186045419}
-                         #:db{:id 17592186045420}]}
+  ;; GETTING THE IDENT OF AN ENUM
+  (dm/q '[:find ?type-ident
+          :where
+          [?e :policy/session ?s]
+          [?s :session/entity ?entity]
+          [?entity :db/ident ?type-ident]
+          [?e :policy/projection ?p]
+          [?p :projection/id ?projection-id]
+          :in $ ?projection-id]
+        mdb #uuid "a975be9f-6ab6-4df1-8036-57a5be9ecb13")
 
   )
