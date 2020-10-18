ab6a9987f5a04ea4f77b3d0458643b8a21279a91
motform <love.lagerkvist@protonmail.com>
2020-10-18 19:48:44 +0200

    Add (long term) sessions and temp storage to app-db

--- src/amr/app/db.cljs
+++ src/amr/app/db.cljs
@@ -10,28 +10,38 @@
 ;; TODO co-locate the schema and the initial-db?
 (def default-db-schema
   [:map
+
    [:app
     [:map
      [:route string?]
-     [:author-id uuid?]]]
+     [:author uuid?]]]
+
+   [:sessions
+    [:map-of :uuid?
+     [:map
+      [:entity model/entity]
+      [:policy model/policy]
+      [:effect model/effect]]]]
+
    [:game
     [:session-id uuid?]
     [:entity? model/entity]
     [:cards [:vector qualified-keyword?]]
     [:policy [:map]]
     [:reflection [:map]]
-    [:entites [:map]]]])
+    [:entites [:map]]]
+
+   [:temp [:map [:projections map?]]]])
 
 (def default-db
   {:app  {:route nil
-          :author-id (random-uuid)}
-   :game {:cards [:card/intro :card/select-entity]
-          :session-id (random-uuid)
-          :entity nil
-          :content {:projection {}
-                    :policy {}
-                    :reflection {}}
-          :entities #:entity{:aqua 8 :flora 2 :fauna 7 :homo-sapiens 5 :bacterica 2}}})
+          :pending-request? false
+          :author (random-uuid)}
+   :sessions {}
+   :game {:screen :screen/intro
+          :current-session nil
+          :entities #:entity{:aqua 8 :flora 2 :fauna 7 :homo-sapiens 5 :bacterica 2}}
+   :temp {:projections {}}})
 
 ;;; LOCAL-STORAGE
 
