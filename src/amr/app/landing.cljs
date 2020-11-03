782983be8692a66f6f1eae70b7b2ebb03214a526
motform <love.lagerkvist@protonmail.com>
2020-11-03 10:54:08 +0100

    Update wording

--- src/amr/app/landing.cljs
+++ src/amr/app/landing.cljs
@@ -15,13 +15,12 @@
 (defn hero []
   (let [selected-entity (r/atom nil)]
     (fn []
-      [:section.col.landing-hero {:class @selected-entity}
-       [:div.content 
-        [:h1 "Welcome to the Parliament of Species, a platform where we collectively imagine the future of antibiotic-resistant bacteria. "]
-        [:p "The year is 2030 — " [:em "antibiotic-resistant bacteria"] " have started causing infections that are not treatable with antibiotics. These bacteria are rapidly spreading across the globe threatening the ability to treat common infections. "]
-        [:p "The " [:em "Parliament of Species"] ", consisting of the Entities " [entity "aqua" 1 selected-entity] "," [entity "flora" -2 selected-entity] "," [entity "fauna" -1 selected-entity] "&" [entity "homo-sapiens" 2 selected-entity] " was created to tackle antibiotic-resistant bacteria and its impact at a global level."]
-        [:p "In order to coexist, the entities must find a balance between their wellbeing, while managing the level of antibiotic-resistant bacteria. Your role as an entity of The Parliament of Species is to maintain this balance through collaborative policy making."]
-        [:p "The balance of the wellbeing of the entities is displayed in the coloured bar at the top of the screen, as well as the level of antibiotic-resistant bacteria. If the level of resistance increases past the global threshold, " [:em "The Parliament of Species"] " is reset and the Policy Proposals that caused the largest negative impact on all of the entities are discrded."]]
+      [:section.col.centered.landing-hero {:class @selected-entity}
+       [:h1.narrow "Welcome to the Parliament of Species, a platform where we collectively imagine the future of antibiotic-resistant bacteria. "]
+       [:p.text "The year is 2030 — " [:em "antibiotic-resistant bacteria"] " have started causing infections that are not treatable with antibiotics. These bacteria are rapidly spreading across the globe threatening the ability to treat common infections. "]
+       [:p.text "The " [:em "Parliament of Species"] ", consisting of the Entities " [entity "aqua" 1 selected-entity] "," [entity "flora" -2 selected-entity] "," [entity "fauna" -1 selected-entity] "&" [entity "homo-sapiens" 2 selected-entity] " was created to tackle antibiotic-resistant bacteria and its impact at a global level."]
+       [:p.text "In order to coexist, the entities must find a balance between their wellbeing, while managing the level of antibiotic-resistant bacteria. Your role as an entity of The Parliament of Species is to maintain this balance through collaborative policy making."]
+       [:p.text "The balance of the wellbeing of the entities is displayed in the coloured bar at the top of the screen, as well as the level of antibiotic-resistant bacteria. If the level of resistance increases past the global threshold, " [:em "The Parliament of Species"] " is reset and the Policy Proposals that caused the largest negative impact on all of the entities are discrded."]
        [:a.landing-visit {:href (href :route/policymaking)}
         "Participate in the Parliament"]])))
 
@@ -59,5 +58,4 @@
 
 (defn landing []
   [:main.landing.col 
-   [hero]
-   ])
+   [hero]])
